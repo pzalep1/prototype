e0834a8c0f897d79acfa49415f918e053f191646
diegosoto9744@gmail.com <you@example.com>
2022-07-18 13:17:06 -0400

    Note and File in progress

--- src/entities/Files/createFile.ts
+++ src/entities/Files/createFile.ts
@@ -1,6 +1,10 @@
+
+//import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { IsDate, IsString, IsNotEmpty, ValidateNested } from 'class-validator';
+
 export class fileCreateDTO
  {
-
+    IsString()
     public id: string;
 
     public date: Date;
