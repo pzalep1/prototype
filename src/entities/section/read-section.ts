0ed7bb5d2bd341fa8a16653f2f250bceaf22e382
kfrancois <kellel14@gmail.com>
2022-07-18 15:27:52 -0400

    patch route

---
+++ src/entities/section/read-section.ts
@@ -0,0 +1,9 @@
+export class SectionReadDTO{
+users: String;
+location: string;
+notes: string;
+files:string;
+creator: string;
+date: string;
+header: string;
+}
--- src/modules/section/section.controller.ts
+++ src/modules/section/section.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Section } from 'src/entities/section/section';
 
 @Controller()
@@ -9,11 +9,26 @@ export class SectionController {
     @InjectModel(Section.name) private sectionModel: Model<any> 
     ) {}
 
-  @Post('/Sections')
-  @UsePipes(ValidationPipe)
-  async getSection(@Body() sectionUpdates: Section): Promise<string> {
-    const sections = new this.sectionModel(this.sectionModel);
-    await sections.save();
-    return 'User Created!'
-  }
+    @Post('/Sections')
+    @UsePipes(ValidationPipe)
+    async getSection(@Body() sectionUpdates: Section): Promise<string> {
+        const sections = new this.sectionModel(this.sectionModel);
+        await sections.save();
+        return 'Section Created!'
+    }
+
+    @Patch('section/:sectionid')
+    async UpdateSection(@Param('sectionId') sectionID: string, @Body() update: Section): Promise<any>{
+        if(!Types.ObjectId.isValid(sectionID)) {
+        throw new HttpException('SectionId is not a valid mongo id', HttpStatus.BAD_REQUEST);
+        }
+
+        const section = await this.sectionModel.findOne({ _id: new Types.ObjectId(sectionID)});
+        if(!section) {
+        throw new HttpException('Section was not found', HttpStatus.NOT_FOUND);
+        }
+
+        return await this.sectionModel.findOneAndUpdate({ _id: new Types.ObjectId(sectionID) }, { ...update });
+    }
+
 }
