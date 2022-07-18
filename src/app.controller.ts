2298cbe739af748842adbe1dca56914ef34280b3
Paige Zaleppa <pz0342@gmail.com>
2022-07-18 13:54:36 -0400

    Mongoose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Patch, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { AppService } from './app.service';
+import { User } from './entities/user/user';
 import { UserUpdateDTO } from './entities/user/userUpdateDTO';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectModel(User.name) private userModel: Model<any> 
+    ) {}
 
   @Patch()
   @UsePipes(ValidationPipe)
-  getHello(@Body() userUpdates: UserUpdateDTO): string {
-    console.log(userUpdates)
-    return this.appService.getHello();
+  async getHello(@Body() userUpdates: UserUpdateDTO): Promise<string> {
+    const user = new this.userModel(userUpdates);
+    await user.save();
+    return 'User Created!'
   }
 
   @Get('home')
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User, UserSchema } from './entities/user/user';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost/inso',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }
+    ),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
