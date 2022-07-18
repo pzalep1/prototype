523f595dd3a2b134815fa1f8294175971b1d3865
Paige Zaleppa <pz0342@gmail.com>
2022-07-18 12:19:22 -0400

    :sparkles: Adding stuff for users and schedules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,5 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('home')
+  getHome(): string {
+    return 'home is working';
+  }
 }
 ``
--- src/entities/schedule/schedule.ts
+++ src/entities/schedule/schedule.ts
@@ -1,3 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Types } from "mongoose";
+
+export type ScheduleDocument = Schedule & Document;
+
+@Schema()
 export class Schedule {
-    
+    @Prop()
+    date: Date;
+
+    @Prop()
+    creator: Types.ObjectId;
+
+    @Prop()
+    location: string;
+
+    constructor(partial: Partial<Schedule>) {
+        Object.assign(this, partial);
+    }
+
 }
+
+export const ScheduleSchema = SchemaFactory.createForClass(Schedule);
