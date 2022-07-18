c84e4e8ad70f66a3d81dfe1d5929968a4ec13589
Paige Zaleppa <pz0342@gmail.com>
2022-07-18 15:13:35 -0400

    :sparkles: Add patch

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { UserUpdateDTO } from 'src/entities/user/userUpdateDTO';
 import { User } from '../../entities/user/user';
 import { UserWriteDTO } from '../../entities/user/userWriteDTO';
 
@@ -18,10 +19,18 @@ export class UserController {
     return 'User Created!'
   }
 
-  @Get("users/:userid")
+  @Patch('users/:userid')
+  async UpdateUser(@Param('userid') userId: string, @Body() update: UserUpdateDTO): Promise<any>{
+    if(!Types.ObjectId.isValid(userId)) {
+      throw new HttpException('UserId is not a valid mongo id', HttpStatus.BAD_REQUEST);
+    }
 
-  async UpdateUser(@Param('userid') userid:string): Promise<any>{
-    console.log("userid")
+    const user = await this.userModel.findOne({ _id: new Types.ObjectId(userId)});
+    if(!user) {
+      throw new HttpException('User was not found', HttpStatus.NOT_FOUND);
+    }
+
+    return await this.userModel.findOneAndUpdate({ _id: new Types.ObjectId(userId) }, { ...update });
 
   }
 }
